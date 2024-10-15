82c3f10e57aa1cfc01b42efae28e8b10f7970004
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-15 14:03:53 +0200

    add rules to avoid output repetitions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 //Stampo i numeri da 1 a 100
 //stampo multipli di 3
-//stampo multipli di 5 
+//stampo multipli di 5
 //stampo i multipli comuni
 const suono1 = "FIZZ!";
 const suono2 = "BUZZ!";
 const suono3 = "FIZZBUZZ!";
-for(let i=0; i<100; i++){
-    let num = i+1;
-    let multipliDiTre = num % 3;
-    let multipliDiCinque = num % 5;
-    if(multipliDiTre === 0){
-        console.log(num+" è multiplo di 3: "+suono1);
-    }
-    if(multipliDiCinque === 0){
-        console.log(num+" è multiplo di 5: "+suono2);
-    }
+for (let i = 0; i < 100; i++) {
+  let num = i + 1;
+  let multipliDiTre = num % 3;
+  let multipliDiCinque = num % 5;
+  let flag = false;
+  if (multipliDiTre === 0 && multipliDiCinque === 0) {
+    console.log(num + " è multiplo di entrambi: " + suono3);
+    flag = true;
+  }
+  if (multipliDiTre === 0 && !flag) {
+    console.log(num + " è multiplo di 3: " + suono1);
+  }
+  if (multipliDiCinque === 0 && !flag) {
+    console.log(num + " è multiplo di 5: " + suono2);
+  }
 }
